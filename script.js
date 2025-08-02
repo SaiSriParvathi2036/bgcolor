3c3a0a3f860387ede5a9ea874492268475e12257
SaiSriParvathi2036 <damasanisaisriparvathi@gmail.com>
2025-08-02 12:32:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const Button = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+const hex = ['0','1','2','3','4','5','6','7','8','9','a','c','d','e','f'];
+
+Button.addEventListener("click" ,()=>{
+    const hexcolor = generatehexcolor();
+    document.body.style.backgroundColor =
+    hexcolor;
+    colorText.textContent = hexcolor;
+}
+);
+
+function generatehexcolor(){
+    let hexColor = "#"
+    for(i=0;i<6;i++){
+        hexColor = hexColor + hex
+        [generateRandomNumber()]
+    }
+    return hexColor;
+};
+
+function generateRandomNumber (){
+    return Math.floor(Math.random() * hex.length)
+};
